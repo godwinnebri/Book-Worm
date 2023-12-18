8de0973a522b4c63e306dec9345559e467d7d14d
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-12-18 17:48:22 +0100

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Godwin IE on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
